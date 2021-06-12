61b2a0b4e7a28511ad1af8148f3c1edb730c609d
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-06-12 12:20:55 +0300

    Add page html css and js files

---
+++ static/js/akan.js
@@ -0,0 +1,25 @@
+function submission () {
+    let day = document.getElementsByClassName("day").value;
+    let dd = parseInt(day);
+    let month = document.getElementsByClassName ("month").value;
+    let mm = parseInt(day);
+    let year = document.getElementById("year").value;
+    let cc = parseInt(year.toString().slice(0,1));
+    let yy = parseInt(year.toString().slice(2,3));
+
+    // formula
+    let result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+    let femaleName = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    if (document.getElementById("gender").checked) {
+        let gender = "male";
+    }
+    else {
+        let gender = "female";
+    }
+    if (dd <= 0 || dd > 31) {
+        alert("invalid entry")
+    }
+}
