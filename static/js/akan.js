5bcf4f86ebb508e66500b62889ab2d82e53e9ec9
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-06-13 18:08:27 +0300

    Add name arrays.

--- static/js/akan.js
+++ static/js/akan.js
@@ -8,6 +8,10 @@ function akanName() {
 
   // cc = year/100
   var cc = parseInt(year.toString().slice(0, 2));
-  
 
+  var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+  // Names arrays
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 }
