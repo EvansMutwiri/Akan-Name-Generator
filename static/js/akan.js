88a1086b529a90a5b79d1c66f84c43c628a36917
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-06-13 18:05:47 +0300

    Define variables from input

--- static/js/akan.js
+++ static/js/akan.js
@@ -1,1 +1,13 @@
-var 
+function akanName() {
+  var day = document.getElementById("day").value;
+  var dd = parseInt(day);
+  var month = document.getElementById("month").value;
+  var mm = parseInt(month);
+  var year = document.getElementById("year").value;
+  var yy = parseInt(year.toString().slice(2));
+
+  // cc = year/100
+  var cc = parseInt(year.toString().slice(0, 2));
+  
+
+}
